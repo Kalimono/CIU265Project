141736dcfb266b5d136d53c8b9465c4e2cb0a444
Kalimono <obeycmb@gmail.com>
2023-10-10 14:01:39 +0200

    Arduino and sound

--- Assets/Scripts/ArduinoCommunications.cs
+++ Assets/Scripts/ArduinoCommunications.cs
@@ -6,20 +6,46 @@ public class DataSender : MonoBehaviour
 {
     private string serverURL = "http://localhost:5000/write"; // Replace with your Flask server URL
 
+    private int currentAngle = 90;
+
     void Start()
     {
-        StartCoroutine(SendDataToServer());
+        StartCoroutine(SendDataToServer(currentAngle));
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     StartCoroutine(SendDataToServer(currentAngle));
+        // }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            StartCoroutine(SendDataToServer());
+            if (currentAngle > 0)
+            {
+                currentAngle -= 10;
+            } else {
+                currentAngle = 0;
+            }
+
+            StartCoroutine(SendDataToServer(currentAngle));
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentAngle < 180)
+            {
+                currentAngle += 10;
+            } else {
+                currentAngle = 180;
+            }
+
+            StartCoroutine(SendDataToServer(currentAngle));
         }
     }
 
-    IEnumerator SendDataToServer()
+    IEnumerator SendDataToServer(int currentAngle)
     {   
         Debug.Log("Sending data to server...");
         // Create a POST request
@@ -27,7 +53,7 @@ public class DataSender : MonoBehaviour
         request.SetRequestHeader("Content-Type", "application/json");
 
         // Create JSON data to send (replace with your data)
-        string jsonData = "{\"key\": \"value\"}";
+        string jsonData = "{\"angle\": " + currentAngle + "}";  
 
         // Convert JSON data to a byte array
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
