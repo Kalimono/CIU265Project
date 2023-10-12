08631660e47ca2b895426c474394f498c6d938e4
Max <maxerik95@gmail.com>
2023-10-12 16:30:07 +0200

    Kaos men på g
    lycka

---
+++ Assets/UI/DragDropPlaymode.cs
@@ -0,0 +1,53 @@
+using UnityEditor;
+using UnityEngine;
+using Unity;
+using UnityEngine.UIElements;
+
+public class DragDropPlaymode : MonoBehaviour
+{
+    
+    UIDocument doc;
+    public void Start()
+    {
+        doc = GetComponent<UIDocument>();
+        // Each editor window contains a root VisualElement object
+        //VisualElement root = rootVisualElement;
+
+        // Import UXML
+        VisualElement root = doc.rootVisualElement;// AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI/DropDownUXML.uxml");
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI/DropDownUXML.uxml");
+        var labelFromUXML = visualTree.Instantiate();
+
+
+        VisualElement questionField = labelFromUXML.Q("label");
+        questionField.style.color = new Color(255, 0, 0);
+        //VisualElement question = new TextElement();
+        TextElement question = new TextElement();
+        question.style.color = new Color(0, 0, 0);
+        question.text = "What is your names??";
+        questionField.Add(question);
+        question.transform.position = new Vector2(15.0f, 50.0f);
+        
+
+        root.Add(labelFromUXML);
+
+        // A stylesheet can be added to a VisualElement.
+        // The style will be applied to the VisualElement and all of its children.
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/UI/DragAndDropWindow.uss");
+        for (int i = 0; i < 5; i++)
+        {
+            var txt = new TextElement();
+            txt.style.color = new Color(0, 0, 0);
+            txt.text = "name" + i.ToString();
+            var ob = new VisualElement();
+            ob.Add(txt);
+            ob.AddToClassList("object");
+            questionField.Add(ob);
+            ob.transform.position = new Vector2(ob.transform.position.x, ob.transform.position.y + (55.0f * i));
+            ob.transform.scale = new Vector3(1, 1.3f - (i * 0.1f), 1);
+            DragAndDropManipulator manip3 = new(ob);
+
+        }
+
+    }
+}
