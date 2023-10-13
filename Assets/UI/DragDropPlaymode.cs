ac37907f1715e2cacccc8bcf2c7192cfcfa317ed
Max <maxerik95@gmail.com>
2023-10-13 13:46:12 +0200

    All good
    now

--- Assets/UI/DragDropPlaymode.cs
+++ Assets/UI/DragDropPlaymode.cs
@@ -5,6 +5,8 @@ using UnityEngine.UIElements;
 
 public class DragDropPlaymode : MonoBehaviour
 {
+
+    //private void 
     
     UIDocument doc;
     public void Start()
@@ -15,20 +17,21 @@ public class DragDropPlaymode : MonoBehaviour
 
         // Import UXML
         VisualElement root = doc.rootVisualElement;// AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI/DropDownUXML.uxml");
-
+        root.style.alignItems = Align.FlexEnd;
+        root.style.flexDirection = FlexDirection.Row;
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI/DropDownUXML.uxml");
         var labelFromUXML = visualTree.Instantiate();
 
 
-        //VisualElement questionField = labelFromUXML.Q("label");
+        VisualElement questionField = labelFromUXML.Q("questionField");
         //questionField.style.color = new Color(255, 0, 0);
         //VisualElement question = new TextElement();
-        //TextElement question = new TextElement();
+        TextElement question = new TextElement();
         //question.style.color = new Color(0, 0, 0);
-        //question.text = "What is your names??";
-        //questionField.Add(question);
-        //question.transform.position = new Vector2(15.0f, 50.0f);
-        
+        question.text = "Put your names in order:\nCalmness in stressful sitiations.";
+        questionField.Add(question);
+        question.transform.position = new Vector2(15.0f, 50.0f);
+
 
         root.Add(labelFromUXML);
 
@@ -40,16 +43,16 @@ public class DragDropPlaymode : MonoBehaviour
         //new(root.Q<VisualElement>("object"));
         for (int i = 0; i < 5; i++)
         {
-            //var txt = new TextElement();
-            //txt.style.color = new Color(0, 0, 0);
-            //txt.text = "name" + i.ToString();
+            var txt = new TextElement();
+            txt.style.color = new Color(0, 0, 0);
+            txt.text = "name" + i.ToString();
             var ob = new VisualElement();
-            //ob.Add(txt);
+            ob.Add(txt);
             ob.AddToClassList("object");
             labelFromUXML.Add(ob);
             //questionField.Add(ob);
             ob.transform.position = new Vector2(ob.transform.position.x, ob.transform.position.y + (55.0f * i));
-            ob.transform.scale = new Vector3(1, 1.3f - (i * 0.1f), 1);
+            //ob.transform.scale = new Vector3(1, 1.3f - (i * 0.1f), 1);
             DragAndDropManipulator manip3 = new(ob);
 
         }
