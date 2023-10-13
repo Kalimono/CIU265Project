7a6ca78e06f2d808e0e1be8b688cd22042257e47
Max <maxerik95@gmail.com>
2023-10-13 09:05:24 +0200

    Fixat playmode
    yes

--- Assets/UI/DragDropPlaymode.cs
+++ Assets/UI/DragDropPlaymode.cs
@@ -15,18 +15,19 @@ public class DragDropPlaymode : MonoBehaviour
 
         // Import UXML
         VisualElement root = doc.rootVisualElement;// AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI/DropDownUXML.uxml");
+
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI/DropDownUXML.uxml");
         var labelFromUXML = visualTree.Instantiate();
 
 
-        VisualElement questionField = labelFromUXML.Q("label");
-        questionField.style.color = new Color(255, 0, 0);
+        //VisualElement questionField = labelFromUXML.Q("label");
+        //questionField.style.color = new Color(255, 0, 0);
         //VisualElement question = new TextElement();
-        TextElement question = new TextElement();
-        question.style.color = new Color(0, 0, 0);
-        question.text = "What is your names??";
-        questionField.Add(question);
-        question.transform.position = new Vector2(15.0f, 50.0f);
+        //TextElement question = new TextElement();
+        //question.style.color = new Color(0, 0, 0);
+        //question.text = "What is your names??";
+        //questionField.Add(question);
+        //question.transform.position = new Vector2(15.0f, 50.0f);
         
 
         root.Add(labelFromUXML);
@@ -34,15 +35,19 @@ public class DragDropPlaymode : MonoBehaviour
         // A stylesheet can be added to a VisualElement.
         // The style will be applied to the VisualElement and all of its children.
         var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/UI/DragAndDropWindow.uss");
+
+        //DragAndDropManipulator manipulator =
+        //new(root.Q<VisualElement>("object"));
         for (int i = 0; i < 5; i++)
         {
-            var txt = new TextElement();
-            txt.style.color = new Color(0, 0, 0);
-            txt.text = "name" + i.ToString();
+            //var txt = new TextElement();
+            //txt.style.color = new Color(0, 0, 0);
+            //txt.text = "name" + i.ToString();
             var ob = new VisualElement();
-            ob.Add(txt);
+            //ob.Add(txt);
             ob.AddToClassList("object");
-            questionField.Add(ob);
+            labelFromUXML.Add(ob);
+            //questionField.Add(ob);
             ob.transform.position = new Vector2(ob.transform.position.x, ob.transform.position.y + (55.0f * i));
             ob.transform.scale = new Vector3(1, 1.3f - (i * 0.1f), 1);
             DragAndDropManipulator manip3 = new(ob);
